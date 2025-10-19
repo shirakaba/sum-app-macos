430516ea609f95fd7f1cd0141758aff806f8f698
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-19 23:58:46 +0900

    quick-and-dirty metro config

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require('node:path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
@@ -6,6 +7,32 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+let config = {};
+config = mergeConfig(getDefaultConfig(__dirname), config);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: {
+    ...config.resolver.extraNodeModules,
+    // 'react-native-node-api': path.resolve(
+    //   __dirname,
+    //   '../react-native-node-api-modules/packages/host',
+    // ),
+    'react-native-node-api': path.resolve(
+      __dirname,
+      './node_modules/react-native-node-api',
+    ),
+  },
+};
+
+config.watchFolders = [
+  __dirname,
+  path.resolve(__dirname, './node_modules'),
+  path.resolve(__dirname, '../sum-lib'),
+  path.resolve(__dirname, '../napi-ios'),
+  path.resolve(__dirname, '../napi-ios/packages/macos'),
+  // path.resolve(__dirname, '../react-native-node-api-modules'),
+  // path.resolve(__dirname, '../react-native-node-api-modules/packages/host'),
+];
+
+module.exports = config;
